0c5f194e1a048b9bb6c3e45324ed50ee3f43c297
Jeun Yun Tsang <paul@Jeuns-MacBook-Pro.local>
2019-02-12 20:47:54 +0900

    add bootstrap js and collapse nav on mobile

--- src/components/template/Header.js
+++ src/components/template/Header.js
@@ -6,12 +6,25 @@ import SearchForm from '../search/SearchForm';
 const Header = () => {
     return (
         <header className="header bg-dark">
-            <div className="container navbar">
+            <nav className="container navbar navbar-dark navbar-expand-md">
                 <Link to="/" className="navbar-brand text-white">
                     Movies and Chill
                 </Link>
-                <SearchForm />
-            </div>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <Link to="/trending" className="nav-link">Trending</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/popular" className="nav-link">Popular</Link>
+                        </li>
+                    </ul>
+                    <SearchForm />
+                </div>
+            </nav>
         </header>
     );
 };
