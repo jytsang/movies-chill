a5774513c49e75b84992d1e524b293daba09a78a
Jeun Yun Tsang <paul@Jeuns-MacBook-Pro.local>
2019-02-13 20:58:52 +0900

    add ScrollToTop component for page changes

--- src/components/App.js
+++ src/components/App.js
@@ -9,28 +9,31 @@ import popular from '../pages/popular';
 import search from '../pages/search';
 import MovieShow from './movies/MovieShow';
 import PersonShow from './cast/PersonShow';
+import ScrollToTop from '../utilities/ScrollToTop';
 
 const App = () => {
     return (
         //use Router for history compatibility
         //use Switch and exact for routes to match correct URL once
         <Router history={history}>
-            <div className="wrapper">
-                <Header />
-                <Switch>
-                    <Route path="/" exact render={() => <Redirect to='/trending'/>} />
-                    <Route path="/trending" exact component={trending} />
-                    <Route path="/trending/:pageNumber" exact component={trending} />
-                    <Route path="/popular" exact component={popular} />
-                    <Route path="/popular/:pageNumber" exact component={popular} />
-                    <Route path="/search/:keyword" exact component={search} />
-                    <Route path="/search/:keyword/:pageNumber" exact component={search} />
-                    <Route path="/movies/:id" exact component={MovieShow} />
-                    <Route path="/person/:id" exact component={PersonShow} />
-                    <Redirect to="/" />
-                </Switch>
-                <Footer />
-            </div>
+            <ScrollToTop>
+                <div className="wrapper">
+                    <Header />
+                    <Switch>
+                        <Route path="/" exact render={() => <Redirect to='/trending'/>} />
+                        <Route path="/trending" exact component={trending} />
+                        <Route path="/trending/:pageNumber" exact component={trending} />
+                        <Route path="/popular" exact component={popular} />
+                        <Route path="/popular/:pageNumber" exact component={popular} />
+                        <Route path="/search/:keyword" exact component={search} />
+                        <Route path="/search/:keyword/:pageNumber" exact component={search} />
+                        <Route path="/movies/:id" exact component={MovieShow} />
+                        <Route path="/person/:id" exact component={PersonShow} />
+                        <Redirect to="/" />
+                    </Switch>
+                    <Footer />
+                </div>
+            </ScrollToTop>
         </Router>
     );
 };
